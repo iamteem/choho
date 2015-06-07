fe3040da0c3b26d59f69434f4b32bdf3f050652a
Tim Medina <iamteem@gmail.com>
2015-06-07 10:06:24 +0800

    Update formatting.

--- 0001/Soln.hs
+++ 0001/Soln.hs
@@ -1,5 +1,6 @@
 soln :: Int -> Int
 soln limit = sum(filter f [1..(limit - 1)])
-  where f x = (x `mod` 3 == 0 || x `mod` 5 == 0)
+  where f x = x `mod` 3 == 0 || x `mod` 5 == 0
 
-main = putStrLn(show(soln 1000))
+main :: IO ()
+main = print soln 1000
--- 0002/Soln.hs
+++ 0002/Soln.hs
@@ -1,5 +1,10 @@
-import Data.List
+import           Data.List
+
+fibs :: [Integer]
 fibs = unfoldr (\(a, b) -> if a < 4000000 then Just (a, (b, a + b)) else Nothing) (1, 2)
+
+ans :: Integer
 ans = sum(filter even fibs)
 
+main :: IO ()
 main = putStrLn(show(ans))
